5b99946edec2e246201cca6bb6bc6ed154ae976e
maxCut <max.mesirow@gmail.com>
2020-10-27 01:00:02 -0700

    changed port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path');
 const app = express()
-const port = 7672
+const port = process.env.PORT || 8080;
 
 const { User } = require("twilio-chat");
 const AccessToken = require('twilio').jwt.AccessToken;
